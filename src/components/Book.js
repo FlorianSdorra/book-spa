b139693bdee5f48bd9253b8fcd5d95a85e6a223b
FlorianSdorra <fsd114@posteo.de>
2020-01-26 20:25:23 +0100

    works how it should/ just for desktop version yet tho

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,20 +1,27 @@
 import React from 'react';
 
 class Book extends React.Component{
+    handleClick = url =>{
+        window.open(url, '_blank')
+    }
+
     render(){
 
-        const {book_image, author, title, description} = this.props.data;
+        const {book_image, author, title, description, buy_links} = this.props.data;
+
+        
 
         return(
             <div className="book">
                 <div className="left">
                     <img src={book_image} alt={book_image}></img>
-                    
                 </div>
                 <div className="right">
                     <h2>{title}</h2>
                     <h3>by:{author}</h3>
                     <p>{description}</p>
+                    <button onClick={()=>this.handleClick(buy_links[3].url)}>Buy from local Book Store</button>
+
                 </div>
             </div>
         )
