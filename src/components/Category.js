43be0ea8ff68a03fa54b53ee45365dc314f87119
FlorianSdorra <fsd114@posteo.de>
2020-01-26 18:03:16 +0100

    fetching categories from NYtimes API works fine

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import Book from './Book'
+
 
 
 class Category extends React.Component{
     render(){
-        const { books } = this.props.data;
-
-        const booksList = books.map(el => (
-          <Book data={el} key={el.book_uri}></Book>
-        ));
+        
         return(
                 <div className="category">
                     <div className="header">
-                        <div className="title"><h1 className="outline">{this.props.data.display_name}</h1></div>
+                        <div className="title"><h3 className="outline">{this.props.data.display_name}</h3></div>
                         <div className="image">
                             <img src={this.props.data.list_image} alt={this.props.data.display_name}></img>
                         </div>
                     </div>
-                    <div className="books-container">{booksList}</div>
+                    <div className="books-container"></div>
                 </div>
         )
         
